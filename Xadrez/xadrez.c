c04cea1acb42836a672b65f95be6ed7a2609181f
Vitoria Paiva <Dev.vitoriapaiva@gmail.com>
2026-04-21 22:24:59 +0000

    Adiciona implementação inicial das movimentações das peças de xadrez

---
+++ Xadrez/xadrez.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// Desafio de Xadrez - MateCheck
+// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
+// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+
+int main() {
+    // Nível Novato - Movimentação das Peças
+
+     int Torre, Rainha, Bispo, i;
+    // mover a torre 5 casas para a direita
+
+    for (int i = 0; i < 5; i++ );
+    {
+        printf("Torre se move para a direita\n"); // Imprime a mensagem para cada movimento da torre
+    }
+    do
+    {
+        printf("Rainha se move para a direita\n"); // Imprime a mensagem para cada movimento da rainha
+    }
+    while(Bispo < 5);
+    {
+        printf("Bispo se move para cima a esquerda\n"); // Imprime a mensagem para cada movimento do bispo
+        Bispo++;
+    }
+
+    
+    return 0;
+}
