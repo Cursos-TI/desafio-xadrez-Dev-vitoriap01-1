18fd9c244b280b9e4c6eff130dc7eb3d985efa27
Vitoria Paiva <Dev.vitoriapaiva@gmail.com>
2026-04-22 21:08:37 +0000

    inclusão do tabuleiro

--- Xadrez novato/xadrez.c
+++ Xadrez novato/xadrez.c
@@ -1,61 +1,136 @@
 #include <stdio.h>
 
+#define TAM 8
+
+// desenho do tabuleiro
+void desenharTabuleiro(int linha, int coluna, char peca) {
+
+    for(int i = 0; i < TAM; i++) {
+        for(int j = 0; j < TAM; j++) {
+
+            if(i == linha && j == coluna) {
+                printf("%c ", peca);
+            } else {
+                printf(". ");
+            }
+
+        }
+        printf("\n");
+    }
+}
+
 int main() {
 
-    
-    // torre - 5 casas para a direita
-    printf("Movimento da Torre:\n\n");
+    int opcao;
+    int linha = 0, coluna = 0; // posição inicial
+    char peca = 'P';
 
-    int i = 0;
-    while(i < 5) {
-        printf("Direita\n");
-        i++;
-    }
-
-    printf("\n");
-
-    // bispo - 5 casas para cima e direita
-    
-    printf("Movimento do Bispo:\n\n");
-
-    int j = 0;
     do {
-        printf("Cima\n");
-        printf("Direita\n");
-        j++;
-    } while(j < 5);
+        printf("\n=== MINI XADREZ 8x8 ===\n");
+        printf("1 - Torre\n");
+        printf("2 - Bispo\n");
+        printf("3 - Rainha\n");
+        printf("4 - Cavalo\n");
+        printf("5 - Sair\n");
 
-    printf("\n");
+        printf("Escolha a peça: ");
+        scanf("%d", &opcao);
 
-    
-    // rainha - 5 casas para a esquerda
-    
-    printf("Movimento da Rainha:\n\n");
+        printf("\n");
 
-    for(int k = 0; k < 5; k++) {
-        printf("Esquerda\n");
-    }
+        switch(opcao) {
 
-    printf("\n");
+            // ================= TORRE =================
+            case 1:
+                peca = 'T';
+                printf("Movendo Torre...\n");
 
-    // cavalo - 2 casas para baixo e 1 casa para a esquerda
-    
-    printf("Movimento do Cavalo:\n\n");
+                for(int i = 0; i < 3; i++) {
+                    if(coluna < TAM - 1) {
+                        coluna++;
+                        printf("Direita\n");
+                        desenharTabuleiro(linha, coluna, peca);
+                        printf("\n");
+                    }
+                }
+                break;
 
-    int passosBaixo = 2;
-    int passosEsquerda = 1;
+            // ================= BISPO =================
+            case 2:
+                peca = 'B';
+                printf("Movendo Bispo...\n");
 
-    for(int x = 0; x < passosBaixo; x++) {
-        printf("Baixo\n");
+                int i = 0;
+                while(i < 3) {
+                    if(linha > 0 && coluna < TAM - 1) {
+                        linha--;
+                        coluna++;
+                        printf("Diagonal (Cima + Direita)\n");
+                        desenharTabuleiro(linha, coluna, peca);
+                        printf("\n");
+                    }
+                    i++;
+                }
+                break;
 
-        if(x == passosBaixo - 1) {
-            int y = 0;
-            while(y < passosEsquerda) {
-                printf("Esquerda\n");
-                y++;
-            }
+            // ================= RAINHA =================
+            case 3:
+                peca = 'R';
+                printf("Movendo Rainha...\n");
+
+                for(int k = 0; k < 3; k++) {
+                    if(coluna > 0) {
+                        coluna--;
+                        printf("Esquerda\n");
+                        desenharTabuleiro(linha, coluna, peca);
+                        printf("\n");
+                    }
+                }
+                break;
+
+            // ================= CAVALO =================
+            case 4:
+                peca = 'C';
+                printf("Movendo Cavalo...\n");
+
+                int passosBaixo = 2;
+                int passosEsquerda = 1;
+
+                for(int x = 0; x < passosBaixo; x++) {
+
+                    if(linha < TAM - 1) {
+                        linha++;
+                        printf("Baixo\n");
+                        desenharTabuleiro(linha, coluna, peca);
+                        printf("\n");
+                    }
+
+                    // loop aninhado
+                    if(x == passosBaixo - 1) {
+                        int y = 0;
+
+                        while(y < passosEsquerda) {
+                            if(coluna > 0) {
+                                coluna--;
+                                printf("Esquerda\n");
+                                desenharTabuleiro(linha, coluna, peca);
+                                printf("\n");
+                            }
+                            y++;
+                        }
+                    }
+                }
+                break;
+
+            case 5:
+                printf("Saindo...\n");
+                break;
+
+            default:
+                printf("Opcao invalida!\n");
         }
-    }
+
+    } while(opcao != 5);
 
     return 0;
 }
